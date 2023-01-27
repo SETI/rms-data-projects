7295220d41a3e687e95a364c34f34dc638d08c65
Emilie Simpson <esimpson5678@gmail.com>
2023-01-27 11:05:54 -0800

    Updated version of pds4_create_bundle_member_index.py has functionality. Still requires error throwing implementation.

--- indextools/pds4_create_bundle_member_index.py
+++ indextools/pds4_create_bundle_member_index.py
@@ -17,12 +17,10 @@ This module creates a index of all .xml and .lblx files within a bundle, sorted
 by data collection term. 
 
 """
-
-import csv
-import os
-
 import argparse
+import csv
 from lxml import objectify
+import os
 
 
 # This exception is raised when a LID belongs to a collection that is not
@@ -32,19 +30,8 @@ class BadLID(Exception):
     'The LID does not contain an accepted collection term'
     pass
 
-collection_term_dict = {}
-bundle_file_paths = []
-bundle_member_index = {}
-collection_terms = []
-fullpaths = []
 
-ns = {'pds':'http://pds.nasa.gov/pds4/pds/v1',
-      'cassini': 'http://pds.nasa.gov/pds4/mission/cassini/v1'}
-
-
-def create_bundle_member_index(directory_path, bundle_structure):
-    
-    bundle_name = directory_path.split('/')[-2]
+def create_bundle_members(directory_path, bundle_structure):
     
     # Not all bundle.xml files within a bundle contain the same bundle member
     # entries, so we need to walk through all instances of bundle.xml.
@@ -55,11 +42,11 @@ def create_bundle_member_index(directory_path, bundle_structure):
                 bundle_file_paths.append(os.path.join(path, file))
                     
         for bundle_file_path in bundle_file_paths: 
-            bundle_file_root = (objectify.parse(bundle_file_path, 
-                                          objectify.makeparser(remove_blank_text=True))
-                                         .getroot())
+            bundle_root = (objectify.parse(bundle_file_path, 
+                                           objectify.makeparser(remove_blank_text=True))
+                                    .getroot())
               
-            bundle_member_entries = bundle_file_root.findall('pds:Bundle_Member_Entry', 
+            bundle_member_entries = bundle_root.findall('pds:Bundle_Member_Entry', 
                                                      namespaces=ns)
             
             # Some files may belong to a subset of collections while existing in a 
@@ -68,71 +55,94 @@ def create_bundle_member_index(directory_path, bundle_structure):
             # within all files named bundle.xml. This ensures that files not
             # present in all subdirectories get sorted regardless of location.
             
-            for bundle_member_entry in bundle_member_entries: 
-                lid_whole = (bundle_member_entry.lid_reference
-                                                .text
-                                                .split(':'))
+            for bundle_member in bundle_member_entries: 
+                lid_whole = (bundle_member.lid_reference
+                                          .text
+                                          .split(':'))
                 collection_term = lid_whole[-1]
                 if collection_term not in collection_terms: 
                     collection_terms.append(collection_term)
-                    collection_term_dict[str(collection_term)] = ({
-                         'Reference Type' : str(bundle_member_entry.reference_type), 
-                         'Member Status' : str(bundle_member_entry.member_status)})
+                    collections[str(collection_term)] = ({
+                         'Reference Type' : str(bundle_member.reference_type), 
+                         'Member Status' : str(bundle_member.member_status)})
+                   
                     
-    first_level_subdirectories = next(os.walk(directory_path))[1]
+# FULLPATHS CREATOR
+def create_bundle_member_index(directory_path, bundle_structure):
     
     #bundle grabber
-    if bundle_structure:
-        for subdirectory in first_level_subdirectories: 
-            fullpath = directory_path + subdirectory
-            for root, dirs, files in os.walk(fullpath): 
-                for file in files: 
-                    if file.endswith(('.xml', '.lblx')): 
-                         fullpaths.append(os.path.join(root, file))
-                break
+    def fullpaths_populate(subdirectory):
+        for root, dirs, files in os.walk(subdirectory): 
+            for file in files: 
+                if file.endswith(('.xml', '.lblx')): 
+                     fullpaths.append(os.path.join(root, file))
+            break
+        return fullpaths
     
-    # bundle set grabber
-    elif not bundle_structure:
-        for subdirectory in first_level_subdirectories: 
-            fullpath = directory_path + subdirectory
-            first_level = next(os.walk(fullpath))[1]
-            for level in first_level:
-                path = fullpath + "/" + level
-                for root, dirs, files in os.walk(path): 
-                    for file in files: 
-                        if file.endswith(('.xml', '.lblx')): 
-                            fullpaths.append(os.path.join(root, file))
-                            print(file)
-                    break
-            
-    for fullpath in fullpaths:
-        lid_root = (objectify.parse(fullpath,
-                                   objectify.makeparser(remove_blank_text=True))
-                                            .getroot())
-        lid = str(lid_root.Identification_Area.logical_identifier)
-        for term in collection_terms:
-            if lid.endswith(term):
-                bundle_member_index[lid] = (
-                    {'LID' : lid,
-                     'Reference Type' : collection_term_dict[term]['Reference Type'],
-                     'Member Status' : collection_term_dict[term]['Member Status'],
-                     'Path' : fullpath})
-            
-            elif not[term in lid for term in collection_terms]: 
-                raise BadLID
+    def index_bundle(list_of_paths):
+        fullpaths_sorted = sorted(list_of_paths)
+        for fullpath in fullpaths_sorted:
+            root = (objectify.parse(fullpath,
+                                    objectify.makeparser(remove_blank_text=True))
+                                             .getroot())
+            lid = str(root.Identification_Area.logical_identifier)
+            for term in collection_terms:
+                if lid.endswith(term):
+                    fullpath = fullpath.replace(directory_path, 
+                                                bundle_name + '/')
+                    bundle_member_index[lid] = (
+                        {'LID' : lid,
+                         'Reference Type' : collections[term]['Reference Type'],
+                         'Member Status' : collections[term]['Member Status'],
+                         'Path' : fullpath})
                 
-    with open('/Users/emiliesimpson/' \
-              'Desktop/' + bundle_name + '_indexed.csv',
-              mode='w') as index_csv: 
-        bundle_member_index_writer = csv.DictWriter(index_csv, 
-                                fieldnames=['LID',
-                                            'Reference Type',
-                                            'Member Status',
-                                            'Path'])
-        bundle_member_index_writer.writeheader()
-        for index in bundle_member_index:
-            bundle_member_index_writer.writerow(bundle_member_index[index])
-        
+                elif not[term in lid for term in collection_terms]: 
+                    raise BadLID
+        return bundle_member_index
+    
+    
+    def file_creator(place):        
+        with open(place + '/bundle_member_index.csv',
+                  mode='w') as index_csv: 
+            bundle_member_index_writer = csv.DictWriter(index_csv, 
+                                    fieldnames=['LID',
+                                                'Reference Type',
+                                                'Member Status',
+                                                'Path'])
+            bundle_member_index_writer.writeheader()
+            for index in bundle_member_index:
+                bundle_member_index_writer.writerow(bundle_member_index[index])
+    
+    
+    first_level_subdirectories = next(os.walk(directory_path))[1]
+
+    if not bundle_structure:
+        for sub in first_level_subdirectories:
+            fullpaths.clear()
+            bundle_member_index.clear()
+            file_location = directory_path + sub
+            first_level_sub = next(os.walk(file_location))[1]
+            for level in first_level_sub:
+                intake = file_location + '/' + level
+                paths = fullpaths_populate(intake)
+                index_bundle(paths)
+                file_creator(file_location)
+    else:
+        for sub in first_level_subdirectories:
+            file_location = directory_path + sub
+            paths = fullpaths_populate(file_location)
+            index_bundle(paths)
+            file_creator(directory_path)
+            
+
+collections = {}
+bundle_file_paths = []
+collection_terms = []
+fullpaths = []
+bundle_member_index = {}
+
+ns = {'pds':'http://pds.nasa.gov/pds4/pds/v1',
+      'cassini': 'http://pds.nasa.gov/pds4/mission/cassini/v1'}
 
 parser = argparse.ArgumentParser()
 parser.add_argument('directorypath', type=str, nargs=1,
@@ -149,4 +159,6 @@ parser.set_defaults(bundletype=True)
 
 args = parser.parse_args()
 
+bundle_name = args.directorypath[0].split('/')[-2]
+create_bundle_members(args.directorypath[0], args.bundletype)
 create_bundle_member_index(args.directorypath[0], args.bundletype)
