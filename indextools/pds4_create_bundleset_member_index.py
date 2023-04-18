940f6455a464dd7184fe88a9d7e2dd81099ce603
Emilie Simpson <esimpson5678@gmail.com>
2023-04-18 11:20:57 -0700

    Made changes to allow more modularity for each function

--- indextools/pds4_create_bundleset_member_index.py
+++ indextools/pds4_create_bundleset_member_index.py
@@ -10,7 +10,7 @@ import csv
 from lxml import objectify
 import os
 
-def bundle_file_paths(bundleset_path):
+def get_bundle_filepaths(bundleset_path):
     """Find and store paths to all bundle.xml files in bundleset.
 
     Generates the paths to bundle.xml files for each bundle within the
@@ -26,14 +26,14 @@ def bundle_file_paths(bundleset_path):
     return bundlepaths
 
 
-def index_bundleset(bundle_path, bundleset_index, bundleset_name):
-    """Populate the bundleset_index dictionary with bundle information.
+def get_member_lid(bundle_path):
+    """Populate the bundleset_member_index dictionary with bundle information.
 
     The path to the bundle.xml file is parsed with lxml.objectify. The
     bundle member entries are found and scraped for their LID. This LID is
-    put into the bundle_member_index dictionary as a key with the same LID
+    put into the bundleset_member_index dictionary as a key with the same LID
     and the filepath to the bundle.xml file as values. The input bundle_path 
-    is the path to the bundle, bundleset_index is the dictionary to be
+    is the path to the bundle, bundleset_member_index is the dictionary to be
     populated, and bundleset_name is the name of the bundleset.
     """
     bundle_root = (objectify.parse(bundle_path,
@@ -43,9 +43,18 @@ def index_bundleset(bundle_path, bundleset_index, bundleset_name):
 
     bundle_lid = str(bundle_root.Identification_Area.logical_identifier.text)
     
-    shortpath = bundleset_name + bundle_path.split(bundleset_name)[-1]
+    return bundle_lid
     
-    bundleset_index[bundle_lid] = ({
+    
+def get_shortpath(bundleset_name, bundle_path): 
+    """Create shortened path from full filepath."""
+    shortpath = bundleset_name + bundle_path.split(bundleset_name)[-1]
+    return shortpath
+    
+    
+def add_to_index(bundle_lid, bundleset_member_index, shortpath):
+    """Add bundle information to index dictionary."""
+    bundleset_member_index[bundle_lid] = ({
         'LID': bundle_lid,
         'Path': shortpath})
 
@@ -53,11 +62,11 @@ def index_bundleset(bundle_path, bundleset_index, bundleset_name):
 def file_creator(bundle_location, bundle_member_index):
     """Create a index file in the bundleset directory.
 
-    This takes bundle_member_index and creates a csv file at the location
+    This takes bbundleset_member_index and creates a csv file at the location
     of bundle_location. The bundle_location is the path leading to the 
     bundle.
     """
-    with open(bundle_location + '/bundleset_index.csv',
+    with open(bundle_location + '/bundleset_member_index.csv',
               mode='w', encoding='utf8') as index_csv:
         bundle_member_index_writer = csv.DictWriter(
             index_csv,
@@ -68,14 +77,16 @@ def file_creator(bundle_location, bundle_member_index):
             bundle_member_index_writer.writerow(bundle_member_index[index])
 
 
-def create_bundle_member_index(dirpath):
-    """Create the bundleset_index.csv file for the bundleset."""
-    bundleset_index = {}
-    bundleset_name = dirpath.split('/')[-1]
-    bundlepaths = bundle_file_paths(dirpath)
+def main():
+    """Create the bundleset_member_index.csv file for the bundleset."""
+    bundleset_member_index = {}
+    bundleset_name = args.directorypath.split('/')[-1]
+    bundlepaths = get_bundle_filepaths(args.directorypath)
     for bundlepath in bundlepaths:
-        index_bundleset(bundlepath, bundleset_index, bundleset_name)
-    file_creator(dirpath, bundleset_index)
+        bundle_lid = get_member_lid(bundlepath)
+        shortpath = get_shortpath(bundleset_name, bundlepath)
+        add_to_index(bundle_lid, bundleset_member_index, shortpath)
+    file_creator(args.directorypath, bundleset_member_index)
         
     
 ns = {'pds': 'http://pds.nasa.gov/pds4/pds/v1',
@@ -88,4 +99,5 @@ parser.add_argument('directorypath', type=str,
 
 args = parser.parse_args()
 
-create_bundle_member_index(args.directorypath)
+if __name__ == '__main__':
+    main()
