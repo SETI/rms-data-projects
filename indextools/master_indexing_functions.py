3b6acc15af57c0d651abace9409d74756ba6089a
Emilie Simpson <esimpson5678@gmail.com>
2023-05-31 15:31:17 -0700

    All functions work and give correct results

--- indextools/master_indexing_functions.py
+++ indextools/master_indexing_functions.py
@@ -1,15 +1,6 @@
-"""Create indexes of bundle members and collections in bundles and bundlesets.
-
-This file contains the shared functions of pds4_create_bundle_member_index.py,
-pds4_create_budnleset_member_index.py, and pds4_create_collection_member_
-index.py. 
-
-"""
-import argparse
 import csv
 from lxml import objectify
 import os
-import sys
 
 
 class FilepathsNotFound(Exception):
@@ -34,7 +25,7 @@ def get_member_filepaths(directory, filename):
                        is raised.
     """
     files_found = []
-    if filename == 'bundle':
+    if 'bundle' in filename:
         level = 2
     else:
         assert filename == 'collection'
@@ -73,16 +64,9 @@ def get_schema(bundlexml_files, namespaces):
                     line = line.replace('"', '')
                     line = line.split('=')
                     namespaces.update({line[0]: line[-1]})
-    return namespaces
 
 
-directory = '/Users/emiliesimpson/Emilie-Data/pds4-holdings/bundles/cassini_vims_cruise/'
-filename = 'bundle'
-
-x = get_member_filepaths(directory, filename)
-
-# In-progress function
-def add_to_index(filepath, filename, namespaces, member_index):
+def add_to_index(filepath, filename, namespaces):
     """Fills index with appropriate contents according to specified filename.
     
     Inputs:
@@ -96,8 +80,9 @@ def add_to_index(filepath, filename, namespaces, member_index):
                             information depending on the filename.
                                 
     """
+    member_index = {}
     if 'bundle' in filename:
-        if filename == 'bundleset':
+        if len(filepath) > 1:
             for file in filepath:
                 bundle_root = (objectify.parse(file,
                                                objectify.makeparser(
@@ -108,71 +93,56 @@ def add_to_index(filepath, filename, namespaces, member_index):
                     'LID': bundle_lid,
                     'Path': file})
         else:
-            assert filename == 'bundle'
-            bundle_root = (objectify.parse(filepath,
-                                           objectify.makeparser(
-                                               remove_blank_text=True))
-                                    .getroot())
-            bundle_member_entries = bundle_root.findall('pds:Bundle_Member_Entry',
-                                                        namespaces=namespaces)
-            for bundle_member_entry in bundle_member_entries:
-                member_lid = bundle_member_entry.lid_reference
-                member_index[member_lid] = {
-                    'LID': member_lid,
-                    'Reference Type': bundle_member_entry.reference_type,
-                    'Member Status': bundle_member_entry.member_status,
-                    'Path': '__'}
-            
+            for file in filepath:
+                bundle_root = (objectify.parse(file,
+                                                   objectify.makeparser(
+                                                       remove_blank_text=True))
+                                            .getroot())
+                bundle_member_entries = bundle_root.findall('pds:Bundle_Member_Entry',
+                                                            namespaces=namespaces)
+                for bundle_member_entry in bundle_member_entries:
+                    member_lid = str(bundle_member_entry.lid_reference)
+                    member_index[member_lid] = {
+                        'LID': member_lid,
+                        'Reference Type': str(bundle_member_entry.reference_type),
+                        'Member Status': str(bundle_member_entry.member_status),
+                        'Path': '__'}
+        
         
     else:
         assert filename == 'collection'
-
-        collection_file_root = (objectify.parse(filepath,
-                                objectify.makeparser(remove_blank_text=True)))
         
-        collection_file = collection_file_root.findall('pds:File_Area_Inventory',
-                                                       namespaces=namespaces)
-
-        collection_product_filename = filepath.replace(filepath.split('/')[-1],
-                                                   collection_file[0].File
-                                                                     .file_name
-                                                                     .text)
+        
+        for file in filepath:
+            collection_file_root = (objectify.parse(file,
+                                    objectify.makeparser(remove_blank_text=True)))
+            
+            collection_file = collection_file_root.findall('pds:File_Area_Inventory',
+                                                           namespaces=namespaces)
     
-        with open(collection_product_filename, 'r') as collection_prod_file:
-            lines = collection_prod_file.readlines()
-            for line in lines:
-                parts = line.split(',')
-                lidvid = parts[-1].strip()
-                lid = lidvid.split('::')[0]
-                vid = lidvid.split('::')[-1]
-                if lid == vid:
-                    vid = ''
-                member_index[str(lidvid)] = {
-                    'LID': lid,
-                    'VID': vid,
-                    'Member Status': parts[0],
-                    'Path': '__'}
-                
-                
-def shortpaths(directory, bundle_name, member_index, fullpath):
-    """Shorten the paths in the member_index dictionary.
-    
-    Inputs:
-        directory            The path to the directory.
-
-        subdirectory_name    The name of the source directory.
-
-        member_index         The dictionary of indexed information.
-
-        fullpath             The original path of the data file.
-    """
-    for key in member_index:
-        fullpath = member_index[key]['Path']
-        shortpath = fullpath.replace(directory, bundle_name)
-        member_index[key]['Path'] = shortpath
-
-
-def fullpaths_populate(directory, level):
+            collection_product_filename = file.replace(file.split('/')[-1],
+                                                       collection_file[0].File
+                                                                         .file_name
+                                                                         .text)
+        
+            with open(collection_product_filename, 'r') as collection_prod_file:
+                lines = collection_prod_file.readlines()
+                for line in lines:
+                    parts = line.split(',')
+                    lidvid = parts[-1].strip()
+                    lid = lidvid.split('::')[0]
+                    vid = lidvid.split('::')[-1]
+                    if lid == vid:
+                        vid = ''
+                    member_index[str(lidvid)] = {
+                        'LID': lid,
+                        'VID': vid,
+                        'Member Status': parts[0],
+                        'Path': '__'}
+    return member_index
+                    
+                    
+def fullpaths_populate(directory, filename):
     """Generate the fullpaths to .xml and .lblx files within a subdirectory.
 
     Any instance of .xml and .lblx files within the chosen level of
@@ -187,6 +157,11 @@ def fullpaths_populate(directory, level):
         fullpaths    The list to be populated with filepaths.
     """
     fullpaths = []
+    if filename == 'bundle':
+        level = 2
+    else:
+        assert filename == 'collection'
+        level = 3
     directory = os.path.abspath(directory)
     for root, dirs, files in os.walk(directory):
         if root.count(os.sep) - directory.count(os.sep) < level:
@@ -197,47 +172,17 @@ def fullpaths_populate(directory, level):
     if fullpaths == []:
         raise FilepathsNotFound('No files ending in ".xml" or ".lblx" could '
                                 'be found in the given levels.')
-
-
-
-
-# NOT TESTED YET
-def file_creator(directory, file_name, member_index):
-    """Create the file of the results.
-    
-    Inputs:
-        directory    The path to the directory.
         
-        file_name    The keyword to determine the contents.
-        
-        member_index    The index of bundle member/collection product
-                        information.
-    """
-    fieldnames = {}
-    # These are kept here until they have a place within the index population
-    # functions.
-    fieldnames['collection'] = ['LID', 'VID', 'Member Status', 'Path']
-    fieldnames['bundle'] = ['LID', 'Reference Type', 'Member Status', 'Path']
-    fieldnames['bundleset'] = ['LID', 'Path']
-    index_name = file_name+'_member_index.csv'
-    with open(os.path.join(directory, index_name),
-              mode='w', encoding='utf8') as index_file:
-        member_index_writer = csv.DictWriter(
-            index_file,
-            fieldnames=fieldnames[file_name])
-        member_index_writer.writeheader()
-        for index in sorted(member_index):
-            member_index_writer.writerow(member_index[index])
+    return fullpaths
 
 
-# Still needs docstring
 def match_lids_to_files(fullpaths, filename, member_index):
+    """Match the LID of a file to its counterpart in the dictionary.
+    
+    """
     fullpaths_sorted = sorted(fullpaths)
-    if 'bundle' in filename:
-        if filename == 'bundleset':
-            pass
-        else:
-            assert filename == 'bundle'
+    if filename == 'bundle':
+        if any('__' in entry['Path'] for entry in member_index.values()):
             for path in fullpaths_sorted:
                 bundle_root = (objectify.parse(path,
                                                objectify.makeparser(
@@ -249,20 +194,64 @@ def match_lids_to_files(fullpaths, filename, member_index):
                 else:
                     print(f'PDS4 label found but not a member of this bundle: '
                           f'{path}, {lid}')
+        else:
+            pass
 
     else:
-        assert filename == 'collections'
-        collection_root = (objectify.parse(path,
-                                       objectify.makeparser(
-                                           remove_blank_text=True))
-                                .getroot())
-        lid = str(collection_root.Identification_Area.logical_identifier.text)
-        if lid in member_index:
-            member_index[lid]['Path'] = path
-        else:
-            print(f'PDS4 label found but not a member of this bundle: '
-                  f'{path}, {lid}')
-    return member_index
+        assert filename == 'collection'
+        for path in fullpaths:
+            collection_root = (objectify.parse(path,
+                                           objectify.makeparser(
+                                               remove_blank_text=True))
+                                    .getroot())
+            lid = str(collection_root.Identification_Area.logical_identifier)
+            vid = str(collection_root.Identification_Area.version_id)
+            lidvid = lid + '::' + vid
+            if lidvid in member_index:
+                member_index[lidvid]['Path'] = path
+            else:
+                print(f'PDS4 label found but not a member of this bundle: '
+                      f'{path}, {lid}')
 
 
+def shortpaths(directory, bundle_name, member_index):
+    """Shorten the paths in the member_index dictionary.
+    
+    Inputs:
+        directory            The path to the directory.
 
+        subdirectory_name    The name of the source directory.
+
+        member_index         The dictionary of indexed information.
+    """
+    for key in member_index:
+        fullpath = member_index[key]['Path']
+        shortpath = fullpath.replace(directory, bundle_name + '/')
+        member_index[key]['Path'] = shortpath
+        
+        
+def file_creator(directory, filename, member_index):
+    """Create the file of the results.
+    
+    Inputs:
+        directory    The path to the directory.
+        
+        file_name    The keyword to determine the contents.
+        
+        member_index    The index of bundle member/collection product
+                        information.
+    """
+    # These are kept here until they have a place within the index population
+    # functions.
+    __ = list(member_index.keys())[0]
+    labels = list(member_index[__].keys())
+    index_name = filename+'_member_index.csv'
+    with open(os.path.join(directory, index_name),
+              mode='w', encoding='utf8') as index_file:
+        member_index_writer = csv.DictWriter(
+            index_file,
+            fieldnames=labels)
+        member_index_writer.writeheader()
+        for index in sorted(member_index):
+            member_index_writer.writerow(member_index[index])
+            
