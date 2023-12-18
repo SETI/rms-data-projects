fb9d70fee3b08aa28ecd3ca046da9cb7e3fde6c2
Emilie Simpson <esimpson@seti.org>
2023-12-18 12:06:46 -0800

    Implementing changes according to pull request

--- indextools/indexing_tool.py
+++ indextools/indexing_tool.py
@@ -6,19 +6,11 @@ user-defined XML elements, and generates a CSV index file. The script provides o
 for customizing the extraction process, such as specifying XPath headers, limiting
 search levels, and selecting elements to scrape.
 
-Dependencies:
-- Python 3.x
-- pathlib
-- lxml
-- pandas
-- re
-- argparse
-
 Usage:
     python xml_bundle_scraper.py <directorypath> <pattern>
         [--nlevels NLEVELS]
         [--elements-file ELEMENTS_FILE]
-        [--filesuffix FILESUFFIX]
+        [--file-suffix FILE_SUFFIX]
         [--xpaths]
 
 Arguments:
@@ -27,21 +19,20 @@ Arguments:
     --nlevels NLEVELS    The number of subdirectory levels to search (default: unlimited).
     --elements-file ELEMENTS_FILE
                          Optional text file containing elements to scrape.
-    --filesuffix FILESUFFIX
+    --file-suffix FILE_SUFFIX
                          The type of label file present within the collection (default: 'xml').
     --xpaths             Activate XPath headers in the final index file.
 
 Example:
-    python xml_bundle_scraper.py /path/to/bundle_directory "*.xml" --nlevels 2
-        --elements-file elements.txt --filesuffix xml --xpaths
+    python xml_bundle_scraper.py /path/to/bundle_directory '*.xml' --nlevels 2
+        --elements-file elements.txt --file-suffix xml --xpaths
 """
 
-
-from pathlib import Path
+import argparse
 from lxml import etree
 import pandas as pd
+from pathlib import Path
 import re
-import argparse
 
 
 def convert_header_to_tag(path, root, namespaces):
@@ -49,9 +40,7 @@ def convert_header_to_tag(path, root, namespaces):
 
     Inputs:
         path          XPath expression.
-
         root          The root element of the XML document.
-
         namespaces    Dictionary of XML namespace mappings.
 
     Returns:
@@ -67,9 +56,7 @@ def convert_header_to_xpath(root, xpath_find, namespaces):
 
     Inputs:
         root           The root element of the XML document.
-
         xpath_find     Original XML header path.
-
         namespaces     Dictionary of XML namespace mappings.
 
     Returns:
@@ -91,10 +78,8 @@ def get_member_files(directory, nlevels, regex):
 
     Inputs:
         directory    The directory to start the search.
-
         nlevels      The maximum number of levels to search (set to None for unlimited
                      levels).
-
         regex        Regular expression pattern for file name matching.
 
     Returns:
@@ -118,31 +103,26 @@ def process_tags(xml_results, key, root, namespaces, prefixes, args):
 
     Inputs:
         xml_results    A dictionary containing XML data to be processed.
-
         key            The key representing the XML tag to be processed.
-
         root           The root element of the XML tree.
-
         namespaces     A dictionary containing XML namespace mappings.
-
         prefixes       A dictionary containing XML namespace prefixes.
-
         args           Command-line arguments.
     """
     if args.xpaths:
         key_new = convert_header_to_xpath(root, key, namespaces)
-        for space in list(prefixes.keys()):
-            if space in key_new:
+        for namespace in prefixes.keys():
+            if namespace in key_new:
                 key_new = key_new.replace(
-                    '{'+space+'}', prefixes[space]+':')
+                    '{'+namespace+'}', prefixes[namespace]+':')
         xml_results[key_new] = xml_results[key]
         del xml_results[key]
     else:
         key_new = convert_header_to_tag(key, root, namespaces)
-        for space in list(prefixes.keys()):
-            if space in key_new:
+        for namespace in prefixes.keys():
+            if namespace in key_new:
                 key_new = key_new.replace(
-                    '{'+space+'}', prefixes[space]+':')
+                    '{'+namespace+'}', prefixes[namespace]+':')
         xml_results[key_new] = xml_results[key]
         del xml_results[key]
 
@@ -152,14 +132,10 @@ def search_files(directory, current_level, nlevels, regex, file_paths):
 
     Inputs:
         directory        The directory to start the search.
-
         current_level    The current depth level in the directory structure.
-
         nlevels          The maximum number of levels to search (set to None for
                          unlimited levels).
-
         regex            Regular expression pattern for file name matching.
-
         file_paths       List to store the absolute paths of matching files.
     """
     # Check if the specified number of levels is reached
@@ -183,11 +159,8 @@ def store_element_text(element, tree, results_dict, prefixes):
 
     Inputs:
         element         The XML element.
-
         tree            The XML tree.
-
         results_dict    Dictionary to store results.
-
         prefixes        Dictionary of XML namespace prefixes.
     """
     if element.text and element.text.strip():
@@ -210,19 +183,15 @@ def traverse_and_store(element, tree, results_dict, prefixes, elements_to_scrape
 
     Inputs:
         element               The current XML element.
-
         tree                  The XML tree.
-
         results_dict          Dictionary to store results.
-
         prefixes              Dictionary of XML namespace prefixes.
-
         elements_to_scrape    Optional list of elements to scrape.
     """
     xpath = str(tree.getpath(element))
     tag = str(element.tag)
-    if elements_to_scrape is None or any(tag.endswith("}" + elem) for elem in
-                                         elements_to_scrape):
+    if elements_to_scrape is None or any(tag.endswith("}" + elem)
+                                         for elem in elements_to_scrape):
         store_element_text(element, tree, results_dict, prefixes)
     for child in element:
         traverse_and_store(child, tree, results_dict,
@@ -241,24 +210,32 @@ def write_results_to_csv(results_list, directory):
     for result_dict in results_list:
         lid = result_dict['LID']
         blid = ':'.join(lid.split(':')[:4])
-        bundl = blid.split(':')[-1]
+        bundle = blid.split(':')[-1]
 
         row = {
             'LID': lid,
             'pds:spec': result_dict['pds:spec'],
             'pds:fname': result_dict['pds:fname'],
             'pds:blid': blid,
-            'pds:bundl': bundl
+            'pds:bundle': bundle
         }
         row.update(result_dict['Results'])
         rows.append(row)
 
     df = pd.DataFrame(rows)
 
-    # Reorder columns to have logical_identifier, FileName, FilePath, pds:blid, and pds:bundl
-    columns_order = ['LID', 'pds:fname', 'pds:spec', 'pds:blid', 'pds:bundl'] + \
-        [col for col in df.columns if col not in [
-            'LID', 'pds:fname', 'pds:spec', 'pds:blid', 'pds:bundl']]
+    # Reorder columns to have logical_identifier, FileName, FilePath, pds:blid, and pds:bundle
+    columns_order = (['LID',
+                      'pds:fname',
+                      'pds:spec',
+                      'pds:blid',
+                      'pds:bundle'] + [col for col in df.columns if col not in
+                                       ['LID',
+                                        'pds:fname',
+                                        'pds:spec',
+                                        'pds:blid',
+                                        'pds:bundle']])
+
     df = df[columns_order]
 
     if 'LID' in df.columns:
@@ -270,26 +247,25 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('directorypath', type=str,
                         help='The path to the directory containing the bundle '
-                             'you wish to scrape.')
+                             'you wish to scrape')
 
     parser.add_argument('pattern', type=str,
-                        help='The glob pattern for the files you wish to index.')
+                        help='The glob pattern for the files you wish to index')
 
     # Argument for specifying the number of levels to search
-    parser.add_argument('--nlevels', type=int,
-                        help='The number of subdirectory levels to search (set to None '
-                             'for unlimited levels).')
+    parser.add_argument('--nlevels', type=int, default=None,
+                        help='The number of subdirectory levels to search')
 
     # Argument for specifying elements to scrape from a text file
     parser.add_argument('--elements-file', type=str,
-                        help='Optional text file containing elements to scrape.')
+                        help='Optional text file containing elements to scrape')
 
-    parser.add_argument('--filesuffix', type=str, default='.xml',
+    parser.add_argument('--file-suffix', type=str, default='.xml',
                         help='The type of label file present within the collection')
 
     parser.add_argument('--xpaths', action='store_true',
                         help='A flag that will activate XPath headers in the final '
-                             'index file.')
+                             'index file')
 
     args = parser.parse_args()
 
@@ -300,14 +276,16 @@ def main():
     for directory in directories:
         # Existing code to process each directory...
         directory = directory.resolve()  # Convert to absolute path
-        basedir = directory.parent.resolve()
-        bundle = directory.name
 
         nlevels = args.nlevels
-        regex = r'[\w-]+('+args.filesuffix+')'
+        regex = r'[\w-]+('+args.file_suffix+')'
 
         # Call get_member_files for the current subdirectory
         label_files = get_member_files(directory, nlevels, regex)
+
+        if label_files == []:
+            raise FileNotFoundError(f'No files with suffix {args.file_suffix} found in '
+                                    'directory: {directory}')
 
         if args.elements_file:
             with open(args.elements_file, 'r') as elements_file:
@@ -331,7 +309,7 @@ def main():
                 process_tags(xml_results, key, root,
                              namespaces, prefixes, args)
 
-            lid = xml_results.get('pds:logical_identifier', "Missing_LID")
+            lid = xml_results.get('pds:logical_identifier', 'Missing_LID')
 
             # Append file path and file name to the dictionary
             result_dict = {'LID': lid, 'Results': xml_results,
