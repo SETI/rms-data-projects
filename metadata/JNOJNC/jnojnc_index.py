5a0b9a2bf0ac7df4bc5381d7b1e792fb8059c264
Mark Showalter <mshowalter@seti.org>
2025-04-19 17:51:01 -0700

    JNOJIR metadata updates

--- metadata/JNOJNC/jnojnc_index.py
+++ metadata/JNOJNC/jnojnc_index.py
@@ -13,11 +13,15 @@ metadata tree. The output file is written into the metadata tree with the correc
 subdirectory and name. If a file of that name is already present and there is no backup,
 the existing file is renamed with the ending "-backup.tab".
 
+Afterward, it labels the new index file, regenerates the cumulative index, and labels the
+new cumulative index.
+
 This program does not correct any information that is incorrect in the existing index!
-It also does not write a label.
 """
 
+import glob
 import os
+import pathlib
 import pdstemplate
 import re
 import shutil
@@ -59,6 +63,7 @@ FORMATS = {
     SUB_SPACECRAFT_LONGITUDE: '%8.4f',
 }
 
+
 def write_jnojnc_index(filepath):
 
     # Read the input index
@@ -91,7 +96,7 @@ def write_jnojnc_index(filepath):
         # step. The result of re.split() alternates between the non-dates and the dates.
         parts = ISODATE_REGEX.split(rec)
         for k, part in enumerate(parts):
-            if k%2 == 1:
+            if k % 2 == 1:
                 if part[0] != '"':      # if not already quoted
                     parts[k] = '"' + part + '"'
         rec = ''.join(parts)
@@ -106,20 +111,20 @@ def write_jnojnc_index(filepath):
         volume_id = values[VOLUME_ID]
         if len(volume_id) == 12:
             values[VOLUME_ID] = volume_id[:7] + volume_id[-4:]
-        
+
         # If the three of the four floating-point columns are zero, they should be
         # NOT_APPLICABLE_CONSTANT
-        if values[SPACECRAFT_ALTITUDE:SPACECRAFT_ALTITUDE+3] == [0,0,0]:
+        if values[SPACECRAFT_ALTITUDE:SPACECRAFT_ALTITUDE+3] == [0, 0, 0]:
             values[SPACECRAFT_ALTITUDE] = -1.e32
             values[SUB_SPACECRAFT_LATITUDE] = -1.e32
             values[SUB_SPACECRAFT_LONGITUDE] = -1.e32
 
         # "SPACE" isn't a target!
         if values[TARGET_NAME].rstrip() == 'SPACE':
-            values[TARGET_NAME] == 'N/A'
+            values[TARGET_NAME] = 'N/A'
 
         # Format all columns
-        for k,v in enumerate(values):
+        for k, v in enumerate(values):
             if v == -1.e32:
                 value = '-1.e32'.rjust(LENGTHS[k])
             elif isinstance(v, float):
@@ -127,6 +132,10 @@ def write_jnojnc_index(filepath):
             else:
                 value = '"' + v.rstrip().ljust(LENGTHS[k]) + '"'
             values[k] = value
+
+        # Move FILE_SPECIFICATION_NAME to second column
+        filespec = values.pop(FILE_SPECIFICATION_NAME)
+        values.insert(1, filespec)
 
         # Merge this back into a single record
         new_rec = ','.join(values) + '\r\n'
@@ -146,15 +155,51 @@ def write_jnojnc_index(filepath):
         # Write the new record
         f.write(new_rec.encode('latin8'))
 
-    # Close the output file
+    # Close and label the output file
     f.close()
+    label_jnojnc_index(output_file)
 
-    # Write the label
-    label_path = output_file[:-4] + '.lbl'
-    backup_label = output_file[:-4] + '-original.lbl'
+    return output_file
+
+
+def write_jnojnc_cum_index(index_file):
+
+    # Identify the directory path
+    prefix, _ = VOLUME_ID_REGEX.match(index_file).groups()  # up to volume ID
+
+    # Identify the output file and optional backup
+    index_pattern = prefix + 'JNOJNC_0???/JNOJNC_0???_index.tab'
+    output_file = prefix + 'JNOJNC_0999/JNOJNC_0999_index.tab'
+
+    index_files = glob.glob(index_pattern)
+    index_files.sort()
+    index_files.remove(output_file)     # don't copy self!
+
+    # Make a backup if necessary
+    backup_file = output_file[:-4] + '-backup.tab'
+    if os.path.exists(output_file) and not os.path.exists(backup_file):
+        shutil.move(output_file, backup_file)
+
+    # Open the output file
+    f = open(output_file, 'wb')
+
+    # Loop through the records...
+    for file in index_files:
+        content = pathlib.Path(file).read_bytes()
+        f.write(content)
+
+    # Close and label the output file
+    f.close()
+    label_jnojnc_index(output_file)
+
+
+def label_jnojnc_index(index_file):
+
+    label_path = index_file[:-4] + '.lbl'
+    backup_label = index_file[:-4] + '-backup.lbl'
     if os.path.exists(label_path):
         shutil.move(label_path, backup_label)
-    
+
     program_path = sys.modules['__main__'].__file__
     template_path = os.path.split(program_path)[0] + '/JNOJNC_0xxx_index_template.lbl'
     template = pdstemplate.PdsTemplate(template_path)
@@ -166,6 +211,9 @@ if __name__ == '__main__':
 
     for filepath in sys.argv[1:]:
         print(filepath)
-        write_jnojnc_index(filepath)
+        index_file = write_jnojnc_index(filepath)
+
+    print('merging JNOJNC_0xxx_index_template.tab')
+    write_jnojnc_cum_index(index_file)
 
 ##########################################################################################
