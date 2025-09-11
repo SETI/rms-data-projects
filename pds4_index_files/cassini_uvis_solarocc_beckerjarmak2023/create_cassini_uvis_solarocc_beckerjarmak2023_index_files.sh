da6a6279ffb4eab18fcab9bbb897599185cb4217
Robert French <rfrench@seti.org>
2025-09-11 13:43:04 -0700

    Updates to beckerjarmak scripts

--- pds4_index_files/cassini_uvis_solarocc_beckerjarmak2023/create_cassini_uvis_solarocc_beckerjarmak2023_index_files.sh
+++ pds4_index_files/cassini_uvis_solarocc_beckerjarmak2023/create_cassini_uvis_solarocc_beckerjarmak2023_index_files.sh
@@ -9,5 +9,14 @@ PDS4_HOLDINGS_DIR=$1
 
 BUNDLE=cassini_uvis_solarocc_beckerjarmak2023
 
-mkdir -p $PDS4_HOLDINGS_DIR/metadata/$BUNDLE
-pds4_create_xml_index $PDS4_HOLDINGS_DIR/bundles/$BUNDLE "data/*ingress.xml" "data/*egress.xml" --add-extra-file-info lid,bundle,bundle_lid,filepath --sort-by filepath --fixed-width --simplify-xpaths --limit-xpaths limit_cassini_uvis_solarocc_beckerjarmak2023.txt --dont-number-unique-tags --generate-label ancillary --output-index-file $PDS4_HOLDINGS_DIR/metadata/$BUNDLE/${BUNDLE}_index.csv --rename-headers rename_cassini_uvis_solarocc_beckerjarmak2023.txt
+# mkdir -p $PDS4_HOLDINGS_DIR/metadata/$BUNDLE
+
+pds4_create_xml_index $PDS4_HOLDINGS_DIR/bundles/$BUNDLE/$BUNDLE "data/*ingress.xml" "data/*egress.xml" --add-extra-file-info lid,bundle,bundle_lid,filepath --sort-by filepath --simplify-xpaths --limit-xpaths limit_cassini_uvis_solarocc_beckerjarmak2023.txt --simplify-xpaths --dont-number-unique-tags --rename-headers rename_cassini_uvis_solarocc_beckerjarmak2023.txt --generate-label ancillary --output-index-file ${BUNDLE}_index.csv
+
+# pds4_create_xml_index $PDS4_HOLDINGS_DIR/bundles/$BUNDLE/$BUNDLE "data/*ingress.xml" "data/*egress.xml" --add-extra-file-info lid,bundle,bundle_lid,filepath --sort-by filepath --simplify-xpaths --limit-xpaths limit_cassini_uvis_solarocc_beckerjarmak2023.txt --simplify-xpaths --dont-number-unique-tags --rename-headers rename_cassini_uvis_solarocc_beckerjarmak2023.txt --generate-label ancillary --output-index-file $PDS4_HOLDINGS_DIR/metadata/$BUNDLE/${BUNDLE}_index.csv
+
+# pds4_create_xml_index $PDS4_HOLDINGS_DIR/bundles/$BUNDLE "data/*ingress.xml" "data/*egress.xml" --add-extra-file-info lid,bundle,bundle_lid,filepath --sort-by filepath --fixed-width --limit-xpaths limit_cassini_uvis_solarocc_beckerjarmak2023.txt --dont-number-unique-tags --simplify-xpaths --rename-headers rename_cassini_uvis_solarocc_beckerjarmak2023.txt --generate-label ancillary --output-index-file $PDS4_HOLDINGS_DIR/metadata/$BUNDLE/${BUNDLE}_index.csv
+
+# pds4_create_xml_index $PDS4_HOLDINGS_DIR/bundles/$BUNDLE/$BUNDLE "data/*ingress.xml" "data/*egress.xml" --generate-label ancillary --output-index-file ${BUNDLE}_index.csv --limit-xpaths limit_cassini_uvis_solarocc_beckerjarmak2023.txt --add-extra-file-info lid,bundle,bundle_lid,filepath --sort-by filepath --dont-number-unique-tags --simplify-xpaths --output-headers-file headers.txt --rename-headers rename_cassini_uvis_solarocc_beckerjarmak2023.txt
+
+# pds4_create_xml_index $PDS4_HOLDINGS_DIR/bundles/cassini_uvis_solarocc_beckerjarmak2023 "data/*269_solar_time_series_*gress.xml" --limit-xpaths limit_cassini_uvis_solarocc_beckerjarmak2023_bug2.txt --dont-number-unique-tags --config-file config_cassini_uvis_solarocc_beckerjarmak2023.txt --output-index-file index.csv --output-headers-file headers.txt
